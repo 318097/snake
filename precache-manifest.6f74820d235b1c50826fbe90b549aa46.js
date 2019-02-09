c05089d2fe616a7467c9c9ec2697fa2865f899cf
Mehul <318097@gmail.com>
2019-02-09 15:23:04 +0530

    Updates

---
+++ precache-manifest.6f74820d235b1c50826fbe90b549aa46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c2eac8723aa1d62fc78876d36bb3833",
+    "url": "/snake/static/media/funky-lines.7c2eac87.png"
+  },
+  {
+    "revision": "deba688275e551e0206f081763c6739a",
+    "url": "/snake/static/media/Gugi-Regular.deba6882.ttf"
+  },
+  {
+    "revision": "da1ecade61dd97107e27",
+    "url": "/snake/static/js/runtime~main.da1ecade.js"
+  },
+  {
+    "revision": "250a502d8b8115ee2c92",
+    "url": "/snake/static/js/main.250a502d.chunk.js"
+  },
+  {
+    "revision": "3f0be990edb634e44a8b",
+    "url": "/snake/static/js/1.3f0be990.chunk.js"
+  },
+  {
+    "revision": "250a502d8b8115ee2c92",
+    "url": "/snake/static/css/main.7b87824b.chunk.css"
+  },
+  {
+    "revision": "de39b952c9ec611afa355ce82bd4dbf6",
+    "url": "/snake/index.html"
+  }
+];
