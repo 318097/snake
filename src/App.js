2e5919ab07769fe5298532f20a82b2d35735b07a
Mehul <318097@gmail.com>
2019-04-09 15:18:19 +0530

    feat: husky setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
 import './App.css';
 
 /* Components */
 import Board from './components/Board';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
